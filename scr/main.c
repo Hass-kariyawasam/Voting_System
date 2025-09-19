d31b0786893e62fecc99e2c76e7b43915468573f
Hass Kariyawasam <Hass.kariyawasam@gmail.com>
2025-09-19 10:45:48 +0530

    test a1 admin loging with main

--- scr/main.c
+++ scr/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include "custerm.h"
 
 //funtions declaration
 int main_menu();
-int admin_loging();
+
 int admin_panel();
 int UI_RO();
 int UI_PO();
@@ -17,7 +18,7 @@ int counting_PR_dis_();
 
 int main() {
     // Your code here
-  
+    admin_loging("ELECTION");
     return 0;
 }
 
