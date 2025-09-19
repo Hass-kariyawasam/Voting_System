9ceefd58610c872e915970c52e3f8b2844ec6976
panchali2004 <panchalikaveesha@gmail.com>
2025-09-19 11:02:35 +0530

    add party_2.h

---
+++ scr/party_2.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+         void number() {
+         for (int i = 1; i <= 55; i++) {
+         printf("-");
+        }
+         printf("\n");
+    }
+
+int main() {
+    
+    
+        num();
+    
+    return 0;
+}
