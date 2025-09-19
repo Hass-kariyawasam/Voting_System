43da10dbbf5867c911ac7f9e73608f2181e444a8
panchali2004 <panchalikaveesha@gmail.com>
2025-09-19 10:52:50 +0530

    party admin login and submit nomination list

--- scr/main.c
+++ scr/main.c
@@ -16,70 +16,61 @@ int registration();
 int counting_PR_dis_();
 
 
-int main() {
-    // Your code here
-  
-    return 0;
-}
+int main () {
+      
+    void num();
+    printf("\n\tPARTY ADMIN LOGIN\n\t");
+    void num();
+    printf("\nPlease enter your credentials:");
 
-int main_menu(){
 
-    printf("\nWELCOME TO SRI LANKA PARLIAMENT VOTING SYSTEM 2025");
+                       int Username,Password; 
+                        printf("\nUsername:");
+                        scanf("%d",&Username);
+                        printf("Password: ");
+                        scanf("%d", &Password);
+
+
+                              if (Username == 5615) {
+
+                                     if (Password == 12345) {
+                                     printf("Login Successful!\n");
+                                     void number();
+
+
+                                     void num();
+                                     printf("\n\tPARTY ADMIN CONTROL PANEL\n\t");
+                                     void num();
+                                    
+                                     int choice;
+                                     printf("\n[1] Submit Nomination List\n");
+                                     printf("[2] Logout\n");
+                                     printf("Enter your choice:");
+                                     scanf("%d",&choice);
+
+
+                                      switch (choice)
+                                      {
+                                      case 1:
+                                      printf("Submit Nomination List");
+                                      break;
+                                      case 2:
+                                      printf("Logout");
+                                      break;
+                                      default:
+                                      printf("invalid input");
+
+                                     }
+
+                                     } else {
+                                     printf("Invalid Password\n");
+                                   }
+                              } else {
+                                printf("Invalid Username\n");
+                              }
+
    
-    int number;
-        printf("\n[1] Admin Control Panel\n");
-        printf("[2] Voter Registration\n");
-        printf("[3] Candidate Registration\n");
-        printf("[4] Vote Casting (Polling)\n");
-        printf("[5] View Nominations List\n");
-        
-    	printf("[6] Results & Publications\n");
-        printf("[0] Exit System\n");
-        printf("\nenter your choice:");
-        scanf(" %d",&number);
-    
-        switch (number)
-        {
-            case 0:
-                 printf("Exit System\n");
-                
-                 break;
-            case 1:
-                adimin_menu();
-                 break;
-                 
-
-        }
-    	
+                            
+ return 0;
 }
 
-
-int adimin_menu(){
-
-    printf("ADMIN CONTROL PANEL - 2025\n");
-    	 
-    	 int choice;
-    	     printf("\n[0]Back to main menu\n");
-    	     printf("\n[1]Returing Officer RO)\n");
-    	     printf("\n[2]Election Admin \n");
-    	     printf("\n[3] Presiding Officer (PO) \n");
-    	     printf("\n[4] Party Agent / Observer\n");
-    	     printf("[5] Counting (District Level)n\n");
-    	     
-    	     
-        printf("\nenter your choice:");
-        scanf(" %d",&choice);
-    
-        switch (choice)
-        {
-            case 0:
-            printf("Exit System\n");
-            main_menu();
-            
-            break;
-            case 1:
-        }
-    	 
-
-
-}
